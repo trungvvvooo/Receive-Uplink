c9949efa6d0d4ccecf69e0212a89dcd6452c5711
trungvvvooo <63012733+trungvvvooo@users.noreply.github.com>
2023-05-27 15:44:50 +0700

    Add files via upload

---
+++ project_lte_receiver.gen/sources_1/ip/dds_compiler_0/dds_compiler_0_stub.vhdl
@@ -0,0 +1,33 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+-- Date        : Tue Oct 11 08:14:00 2022
+-- Host        : DESKTOP-FP4AO1S running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               e:/Project/project_lte_receiver/project_lte_receiver.gen/sources_1/ip/dds_compiler_0/dds_compiler_0_stub.vhdl
+-- Design      : dds_compiler_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity dds_compiler_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end dds_compiler_0;
+
+architecture stub of dds_compiler_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,aresetn,m_axis_data_tvalid,m_axis_data_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "dds_compiler_v6_0_21,Vivado 2021.2";
+begin
+end;
